b3424bda014a12750c1a69f6ec45d3f87a0d6588
shailendra <shailendrasethiya@gmail.com>
2025-04-06 15:30:12 +0530

    test cases added

--- cypress/e2e/stringCalculator.cy.js
+++ cypress/e2e/stringCalculator.cy.js
@@ -8,4 +8,16 @@ describe('String Calculator UI', () => {
       cy.get('button').click();
       cy.contains('Result: 0');
     });
+  
+    it('should return the same number if only one number is provided', () => {
+      cy.get('input').clear().type('5');
+      cy.get('button').click();
+      cy.contains('Result: 5');
+    });
+  
+    it('should return the sum of two comma-separated numbers', () => {
+      cy.get('input').clear().type('1,2');
+      cy.get('button').click();
+      cy.contains('Result: 3');
+    });
   });
